7503e6c79ce729c6a508ede865af45c3575bca97
erycaaaaa <eryca847@gmail.com>
2024-11-05 13:01:44 +0700

    Added fitur Splash

---
+++ mrt/lib/presentation/splash_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              width: 700,
+              height: 850,
+              color: Color(0xFF173156),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/tr1.png',
+                      width: 150,
+                      height: 150,
+                    ),
+                    const SizedBox(height: 30),
+                    const Text(
+                      "MRT COMPASS",
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.3,
+              color: Color(0xFFFFAA00),
+              child: Image.asset(
+                'assets/pt1.png',
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
