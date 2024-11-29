7aa3576d4ba331d0f3ddc0d92c8c77461c43933e
ChelaLorenchia <chela.lorenchia277@gmail.com>
2024-11-29 23:58:25 +0700

    pintu keluar

---
+++ mrt/lib/Screens/station_home_screen.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:mrt/constant.dart';
+
+import '../data/station_data.dart'; // Import data stasiun
+import 'station_detail_screen.dart';
+
+class StationDetailHomeScreen extends StatefulWidget {
+  const StationDetailHomeScreen({super.key});
+
+  @override
+  _StationDetailHomeScreenState createState() =>
+      _StationDetailHomeScreenState();
+}
+
+class _StationDetailHomeScreenState extends State<StationDetailHomeScreen> {
+  // Menyimpan daftar semua stasiun
+  List<Map<String, dynamic>> filteredStations = stations;
+
+  // Fungsi untuk memfilter stasiun berdasarkan teks pencarian
+  void _filterStations(String query) {
+    final results = stations.where((station) {
+      final nameLower = station['name'].toLowerCase();
+      final queryLower = query.toLowerCase();
+      return nameLower.contains(queryLower);
+    }).toList();
+
+    setState(() {
+      filteredStations = results;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'INFO PINTU KELUAR',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: kPrimaryColor,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white), // Ikon back
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Cari stasiun',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+              onChanged: _filterStations, // Filter data saat teks berubah
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredStations.length,
+              itemBuilder: (context, index) {
+                final station = filteredStations[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0, vertical: 4.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white, // Latar belakang putih
+                      borderRadius: BorderRadius.circular(12.0),
+                      border: Border.all(
+                        color: Colors.grey, // Border hitam
+                        width: 1.0,
+                      ),
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: kPrimaryHoverColor, // Warna biru ikon
+                        child: Icon(
+                          Icons.train,
+                          color: Colors.white,
+                        ),
+                      ),
+                      title: Text(
+                        station['name'],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black, // Teks hitam
+                        ),
+                      ),
+                      trailing: Icon(Icons.chevron_right),
+                      onTap: () {
+                        // Navigasi ke halaman detail stasiun
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                StationDetailScreen(stationData: station),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
