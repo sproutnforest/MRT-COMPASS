fd8bd46933b7a00ccbdd8b2fdebde533649d1bdf
lusykar1in4 <161692437+lusykar1in4@users.noreply.github.com>
2024-12-01 20:23:45 +0700

    update

--- mrt/lib/Screens/Ticket/buyTicket_Screen.dart
+++ mrt/lib/Screens/Ticket/buyTicket_Screen.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:mrt/Screens/Ticket/ticket_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class TicketPurchaseScreen extends StatefulWidget {
