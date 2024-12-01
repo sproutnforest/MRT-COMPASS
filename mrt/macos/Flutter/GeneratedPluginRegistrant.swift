e0898e33405ab781551fff1645d5fa2d6c6d13e1
mayceren <161692576+mayceren@users.noreply.github.com>
2024-12-01 09:43:42 +0700

    update pub

--- mrt/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mrt/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,6 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 import path_provider_foundation
-import shared_preferences_foundation
-import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -21,6 +19,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
